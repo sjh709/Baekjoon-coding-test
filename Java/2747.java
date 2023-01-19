db4e6d96db2a71cf2159174db1d447918409db5c
Jihee <42454759+sjh709@users.noreply.github.com>
2023-01-19 18:34:35 +0900

    Update 2747.java

--- Java/2747.java
+++ Java/2747.java
@@ -1,23 +1,39 @@
 import java.util.*;
 
 public class Main {
-    public static int[] d;
-    
-    public static int fibo(int n) {
-        if(n == 0 || n == 1)
-            return n;
-        if(d[n] != 0)
-            return d[n];
-        d[n] = fibo(n - 1) + fibo(n - 2);
-        return d[n];
-    }
-    
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        d = new int[n + 1];
-        
-        int answer = fibo(n);
-        System.out.println(answer);
-    }
+	public static int[] dp;
+	public static int fibo(int n) {
+		// 2. 탈출 조건
+		if(dp[n] == -1) // 한번도 연산된 적이 없다면
+			dp[n] = fibo(n - 1) + fibo(n - 2);
+		
+		// 1. 기본 동작
+		return dp[n];
+	}
+	
+	public static void main(String[] args) {
+		// 재귀 함수
+		// 1. 재귀 함수의 기본 동작
+		// 2. 재귀 함수의 탈출 조건
+		
+		Scanner sc = new Scanner(System.in);
+		
+		// ver 2
+//		int n = sc.nextInt();
+//		dp = new int[100];
+//		Arrays.fill(dp, -1);
+//		dp[0] = 0;
+//		dp[1] = 1;
+//		System.out.println(fibo(n));
+		
+		// ver 3
+		int n = sc.nextInt();
+		dp = new int[100];
+		dp[1] = 1;
+		for(int i = 2; i < n + 1; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2];
+		}
+		System.out.println(dp[n]);
+		
+	}
 }
