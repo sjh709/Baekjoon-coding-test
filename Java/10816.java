3ad8db03ee5e11bdb3aa79d7dcdfec7e044b6079
Jihee <42454759+sjh709@users.noreply.github.com>
2023-01-23 20:14:39 +0900

    Create 10816.java

---
+++ Java/10816.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        int n = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < m; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            sb.append(map.getOrDefault(num, 0) + " ");
+        }
+        
+        System.out.println(sb.toString());
+    }
+}
