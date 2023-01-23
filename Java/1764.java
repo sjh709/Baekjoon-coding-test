314065e64a7dcf09bcf26cbd926fa65ed054114c
Jihee <42454759+sjh709@users.noreply.github.com>
2023-01-23 20:45:51 +0900

    Update 1764.java

--- Java/1764.java
+++ Java/1764.java
@@ -5,28 +5,34 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         
-        HashMap<String, Integer> map = new HashMap<>();
+        HashMap<String, Integer> hash = new HashMap<>();
         for(int i = 0; i < n; i++) {
-            String key = br.readLine();
-            map.put(key, map.getOrDefault(key, 0) + 1);
+            hash.put(br.readLine(), 1);
         }
         
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<String> answer = new ArrayList<>();
         for(int i = 0; i < m; i++){
-            String key = br.readLine();
-            int value = map.getOrDefault(key, 0) + 1;
-            if(value >= 2) {
-                list.add(key);
+            String name = br.readLine();
+            if(hash.containsKey(name)) {
+                answer.add(name);
             }
         }
-        Collections.sort(list);
         
-        System.out.println(list.size());
-        for(String s : list) {
-            System.out.println(s);
+        Collections.sort(answer);
+        
+        bw.write(answer.size() + "\n");
+        for(int i = 0; i < answer.size(); i++) {
+            bw.write(answer.get(i));
+            bw.newLine();
         }
+        bw.flush();
+        
+        bw.close();
+        br.close();
     }
 }
