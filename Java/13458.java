58d757630f44695d7d28a8d3c378fc5870398556
Jihee <42454759+sjh709@users.noreply.github.com>
2023-03-14 16:05:27 +0900

    Update 13458.java

--- Java/13458.java
+++ Java/13458.java
@@ -1,1 +1,35 @@
-
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        
+        long answer = 0;
+        for(int i = 0; i < n; i++) {
+            arr[i] -= a;
+            answer++;
+            if(arr[i] <= 0) continue;
+            answer += arr[i] / b;
+            if(arr[i] % b != 0) answer++;
+        }
+        
+        bw.write(String.valueOf(answer));
+        bw.flush();
+        
+        bw.close();
+        br.close();
+    }
+}
