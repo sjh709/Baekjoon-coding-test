8f0c89182830977ea7daaaa47de8e2a7a238aa89
Jihee <42454759+sjh709@users.noreply.github.com>
2023-03-17 16:18:52 +0900

    Update 10101.java

--- Java/10101.java
+++ Java/10101.java
@@ -1,1 +1,32 @@
-
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        int sum = a + b + c;
+        
+        String answer = "";
+        
+        if(a == b && b == c) {
+            answer = "Equilateral";
+        }else if(sum == 180) {
+            if(a == b || b == c || a == c) {
+                answer = "Isosceles";
+            }else {
+                answer = "Scalene";
+            }
+        }else {
+            answer = "Error";
+        }
+        
+        bw.write(answer);
+        bw.flush();
+        
+        bw.close();
+        br.close();
+    }
+}
