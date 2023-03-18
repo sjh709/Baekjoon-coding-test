ef608e9f77156a9e7cf92ca3f3566d6bdf634b73
Jihee <42454759+sjh709@users.noreply.github.com>
2023-03-18 17:43:30 +0900

    Update 2935.java

--- Java/2935.java
+++ Java/2935.java
@@ -1,1 +1,19 @@
-
+import java.io.*;
+import java.math.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        BigInteger A = new BigInteger(br.readLine());
+        String O = br.readLine();
+        BigInteger B = new BigInteger(br.readLine());
+        
+        if(O.equals("+")) bw.write(String.valueOf(A.add(B)));
+        if(O.equals("*")) bw.write(String.valueOf(A.multiply(B)));
+        
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
