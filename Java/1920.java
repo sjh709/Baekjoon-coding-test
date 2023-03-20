f09a59065cafbc6a97f79decede00ab1305a5aac
Jihee <42454759+sjh709@users.noreply.github.com>
2023-03-20 17:08:25 +0900

    Update 1920.java

--- Java/1920.java
+++ Java/1920.java
@@ -1,1 +1,29 @@
-
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        HashMap<Integer, Integer> hash = new HashMap<>();
+        
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            hash.put(num, 1);
+        }
+        
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < m; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            int value = hash.getOrDefault(num, 0);
+            bw.write(String.valueOf(value) + "\n");
+        }
+        bw.flush();
+        
+        bw.close();
+        br.close();
+    }
+}
