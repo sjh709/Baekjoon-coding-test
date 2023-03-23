ac2a53dbd9aa03b67e87b1b0058f617b51000335
Jihee <42454759+sjh709@users.noreply.github.com>
2023-03-23 17:04:38 +0900

    Update 1026.java

--- Java/1026.java
+++ Java/1026.java
@@ -1,26 +1,6 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    public static int min = Integer.MAX_VALUE;
-    public static void combination(int[] a, boolean[] visit, int[] comb, int depth, int[] b) {
-        int answer = 0;
-        if(depth == comb.length) {
-            for(int i = 0; i < comb.length; i++) {
-                answer += comb[i] * b[i];
-            }
-            min = Math.min(min, answer);
-        }
-        
-        for(int i = 0; i < a.length; i++) {
-            if(!visit[i]) {
-                visit[i] = true;
-                comb[depth] = a[i];
-                combination(a, visit, comb, depth + 1, b);
-                visit[i] = false;
-            }
-        }
-    }
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
