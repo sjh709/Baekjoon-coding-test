c0bf60d15442117f0c17a7321144373d864a00cd
Jihee <42454759+sjh709@users.noreply.github.com>
2023-03-25 18:03:03 +0900

    Update 2438.java

--- Java/2438.java
+++ Java/2438.java
@@ -1,1 +1,23 @@
-
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        
+        bw.close();
+        br.close();
+    }
+}
