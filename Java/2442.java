bec4422cf88db46e3871b83700a9e81e5e8ebc7e
Jihee <42454759+sjh709@users.noreply.github.com>
2023-03-26 16:39:11 +0900

    Update 2442.java

--- Java/2442.java
+++ Java/2442.java
@@ -1,1 +1,26 @@
-
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i <= n; i++) {
+            for(int j = 0; j < n - i; j++){
+                sb.append(" ");
+            }
+            for(int j = 1; j <= 2 * i - 1; j++){
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        
+        bw.close();
+        br.close();
+    }
+}
