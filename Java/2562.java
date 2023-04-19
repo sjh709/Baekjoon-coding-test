c3e0fa176219003b6c02063dc4f9e97806eceb00
Jihee <42454759+sjh709@users.noreply.github.com>
2023-04-19 18:58:53 +0900

    Create 2562.java

---
+++ Java/2562.java
@@ -0,0 +1,24 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int max = 0, idx = 0;
+        for(int i = 1; i <= 9; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if(num > max) {
+                max = num;
+                idx = i;
+            }
+        }
+        
+        bw.write(String.valueOf(max));
+        bw.newLine();
+        bw.write(String.valueOf(idx));
+        bw.flush();
+        
+        bw.close();
+        br.close();
+    }
+}
