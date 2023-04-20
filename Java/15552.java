5d9d54691f7286e5e11021bc5c9154b8a8d7c6d4
Jihee <42454759+sjh709@users.noreply.github.com>
2023-04-20 23:30:41 +0900

    Create 15552.java

---
+++ Java/15552.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int t = Integer.parseInt(br.readLine());
+        
+        StringTokenizer st;
+        for(int i = 0; i < t; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.append(String.valueOf(a + b) + "\n");
+        }
+        
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
