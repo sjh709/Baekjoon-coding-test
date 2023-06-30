ac60749bd1a64fd0199dbf5a812e28ebd79b5db0
Jihee <42454759+sjh709@users.noreply.github.com>
2023-06-30 11:25:58 +0900

    Create 1927.java

---
+++ Java/1927.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for(int i = 0; i < n; i++) {
+            int x = Integer.parseInt(br.readLine());
+            
+            if(x == 0) {
+                if(pq.isEmpty()) {
+                    bw.append(0 + "\n");
+                }else {
+                    bw.append(pq.poll() + "\n");
+                }
+            }else {
+                pq.offer(x);
+            }
+        }
+        
+        bw.flush();
+        
+        bw.close();
+        br.close();
+    }
+}
