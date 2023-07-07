f336ccb7ecd5091794b932fbdcf3a423973a0d1c
Jihee <42454759+sjh709@users.noreply.github.com>
2023-07-07 13:32:03 +0900

    Create 2738.java

---
+++ Java/2738.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        
+        int[][] arr1 = new int[n][m];
+        int[][] arr2 = new int[n][m];
+        
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < m; j++) {
+                arr1[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < m; j++) {
+                arr2[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                bw.append(String.valueOf(arr1[i][j] + arr2[i][j]) + " ");
+            }
+            bw.newLine();
+        }
+        
+        bw.flush();
+        
+        bw.close();
+        br.close();
+    }
+}
