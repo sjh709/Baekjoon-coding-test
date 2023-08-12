fd055e93d7b7ba27ec128f24e276c4e3898f5591
Jihee <42454759+sjh709@users.noreply.github.com>
2023-08-12 22:59:34 +0900

    Update 11403.java

--- Java/11403.java
+++ Java/11403.java
@@ -1,1 +1,47 @@
-
+import java.io.*;
+import java.util.*;
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[][] arr = new int[n][n];
+		
+		for(int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < n; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+				
+				if(arr[i][j] == 0) {
+					arr[i][j] = 10000;
+				}
+			}
+		}
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				for(int k = 0; k < n; k++) {
+					arr[j][k] = Math.min(arr[j][k], arr[j][i] + arr[i][k]);
+				}
+			}
+		}
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				if(arr[i][j] == 10000) {
+					bw.append("0 ");
+				}else {
+					bw.append("1 ");
+				}
+			}
+			bw.append("\n");
+		}
+		
+		bw.flush();
+		
+		bw.close();
+		br.close();
+	}
+}
