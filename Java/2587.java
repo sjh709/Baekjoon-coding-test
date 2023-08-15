e15369c82d3823cd32b30924478d079d5ba530d4
Jihee <42454759+sjh709@users.noreply.github.com>
2023-08-15 22:22:41 +0900

    Update 2587.java

--- Java/2587.java
+++ Java/2587.java
@@ -1,1 +1,26 @@
-
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int[] nums = new int[5];
+        int sum = 0;
+        
+        for(int i = 0; i < 5; i++) {
+            nums[i] = Integer.parseInt(br.readLine());
+            sum += nums[i];
+        }
+        
+        Arrays.sort(nums);
+        
+        bw.append(String.valueOf(sum / 5)).append("\n");
+        bw.append(String.valueOf(nums[2]));
+        
+        bw.flush();
+        
+        bw.close();
+        br.close();
+    }
+}
